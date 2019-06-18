1bdd11a10b4a7e2ab9fbffa054c4b7491bcf2cb6
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-06-17 20:49:06 -0700

    Initial Commit

---
+++ ToDoDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ToDoDemo
+//
+//  Created by Prudhvi Gadiraju on 6/17/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
